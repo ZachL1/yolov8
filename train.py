fdc5130ec9ee6b8a455027ad64af509ba0b939f6
zach.duan <2019kkvb@gmail.com>
2023-10-11 18:31:26 +0800

    train script for kaggle

--- train.py
+++ train.py
@@ -1,9 +1,11 @@
 from ultralytics import YOLO
+import os
+# os.environ['CUDA_LAUNCH_BLOCKING']='1'
 
 # Load a model
 # model = YOLO('yolov8n.yaml')  # build a new model from YAML
 # model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
-model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
+model = YOLO('yolov8m.yaml').load('yolov8m.pt')  # build from YAML and transfer weights
 
 # Train the model
-results = model.train(data='mydata/mydata.yaml', epochs=10, imgsz=640, batch=16, device=0, pretrained=True, amp=False, val=True)
+results = model.train(data='/kaggle/working/yolov8/mydata/mydata_kaggle.yaml', epochs=100, imgsz=640, batch=32, device=[0,1], pretrained=True, amp=False, val=True)
